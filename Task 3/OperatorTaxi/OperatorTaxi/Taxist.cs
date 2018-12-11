a5a3a4f084a4e4e8892c9f0746a2addf8f41f4b3
leskovalDima <leskovaldima@gmail.com>
2018-12-11 18:46:12 +0200

    add

---
+++ Task 3/OperatorTaxi/OperatorTaxi/Taxist.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorTaxi
+{
+    public class Taxist
+    {
+        private string carModel;
+        private string number;
+        private bool busy;
+
+        public Taxist() { }
+
+        public Taxist(string CarModel, string Number, bool Busy)
+        {
+            this.CarModel = CarModel;
+            this.Number = Number;
+            this.Busy = Busy;
+        }
+
+        public string CarModel
+        {
+            get
+            {
+                return carModel;
+            }
+            set
+            {
+                carModel = value;
+            }
+        }
+        public string Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                number = value;
+            }
+        }
+        public bool Busy
+        {
+            get
+            {
+                return busy;
+            }
+            set
+            {
+                busy = value;
+            }
+        }
+    }
+}
---
+++ Task 3/OperatorTaxi/OperatorTaxi/Window2.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OperatorTaxi
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+        private void AddOrder(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = (MainWindow)App.Current.MainWindow;
+            if (this.startPoint.Text != "" && this.endPoint.Text != "" && this.count.Text != "")
+            {
+                try
+                {
+                    mainWindow.a.Orders.Add(new Order(this.startPoint.Text, this.endPoint.Text, Convert.ToInt32(this.count.Text), status.NotAppointed, "------"));
+                    this.Close();
+                }
+                catch(Exception)
+                {
+                    MessageBox.Show("Неправильно введені дані");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ви не ввели всі дані");
+            }
+        }
+    }
+}
